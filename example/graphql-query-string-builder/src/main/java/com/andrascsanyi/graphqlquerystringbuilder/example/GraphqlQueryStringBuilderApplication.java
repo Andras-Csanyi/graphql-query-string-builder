5efe1a52a8baba8e4d53097697b4c898379732a8
András Csányi <andras.csanyi@booking.com>
2024-01-19 14:53:06 +0100

    init

---
+++ example/graphql-query-string-builder/src/main/java/com/andrascsanyi/graphqlquerystringbuilder/example/GraphqlQueryStringBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.andrascsanyi.graphqlquerystringbuilder.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlQueryStringBuilderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlQueryStringBuilderApplication.class, args);
+    }
+
+}
